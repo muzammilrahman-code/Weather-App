e17949edfc3ab026813fb71b36fca61dc21af655
Muzammil Rahman <your-email@example.com>
2025-08-03 05:06:30 +0500

    add default search

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,4 +1,4 @@
-import React, {  useRef, useState } from 'react'
+import React, {  useEffect, useRef, useState } from 'react'
 import './Weather.css'
 import search_icon from '../assets/search.png'
 import cloud_icon from '../assets/cloud.png'
@@ -59,14 +59,17 @@ const icon = allIcons[data.weather[0].icon] || clear_icon;
     })
   } catch (error) {
     setWeatherData(false);
-    console.error("Error In Fetching Data");
-    
-    
+    console.error(`Error In Fetching Data : ${error} `);
+  
   }
   
 }
+useEffect(() => {
+  search("London");
+}, []);
 
-return (     <div className='weather'>
+return (    
+   <div className='weather'>
          <div className="search-bar">
          <input ref={inputRef} type="text" placeholder='Search' />
          <img src={search_icon} alt="" onClick={()=> search(inputRef.current.value)} />
